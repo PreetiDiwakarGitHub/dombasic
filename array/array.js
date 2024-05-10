2db3f71ca494033c632a68f3961b682841cf74e7
PreetiDiwakarGitHub <p208016@gmail.com>
2024-05-10 09:07:05 +0530

    first commit

--- array/array.js
+++ array/array.js
@@ -159,3 +159,10 @@
 // )
 // console.log(user);
 
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/boject.git
+// git push -u origin main
+
