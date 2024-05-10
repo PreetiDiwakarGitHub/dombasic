6d1aac1d795ee5364dca375c796e75ad0e2ec8a1
PreetiDiwakarGitHub <p208016@gmail.com>
2024-05-10 09:58:47 +0530

    first commit

---
+++ dom/dom.js
@@ -0,0 +1,55 @@
+// console.log(document.title);
+// console.log(document.body);
+// document.body.style.backgroundColor = "pink"; 
+
+// document.body.childNodes
+
+// document.body.childNodes[1].childNodes
+
+//  let cont = document.body.firstElementChild;
+//  console.log(cont);
+
+// let boxes=document.getElementsByClassName("box");
+// console.log(boxes);
+
+// document.getElementById("red").style.backgroundColor ="red";
+
+// document.querySelectorAll(".box").forEach(elem=>{
+//     console.log(elem);
+// }
+
+// )
+
+// document.getElementsByTagName("Div");
+
+// let e = document.getElementsByTagName("Div")
+// e[3].matches(".box")
+// console.log(e);
+
+// e[1].closest("#red")
+// console.log(e)
+
+// let p = document.querySelector(".container").contains(e[2]);
+// console.log(p);
+
+
+// let p= document.querySelector(".box").innerHTML
+
+// document.querySelectorAll(".box").forEach(elem =>
+    
+// )
+
+// document.querySelector(".box").remove
+
+// document.querySelector(".container").classList;
+
+// document.querySelector(".container").classList.remove("red");
+
+// document.querySelector(".contatner").classList.toggle("red")
+
+// document.querySelector(".contatner").classList.toggle("red")
+
+// document.querySelector(".contatner").classList.toggle("red")
+
+
+
