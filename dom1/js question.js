41898988b339c0f61fe141ec218cc27a009453b0
PreetiDiwakarGitHub <p208016@gmail.com>
2024-05-15 10:13:34 +0530

    question

---
+++ dom1/js question.js
@@ -0,0 +1,61 @@
+// sort Aarry
+
+// let arr = [1,4,2,3,6,7];
+// console.log(arr.sort());
+
+// reverse
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// console.log(arr.reverse());
+
+// order
+
+// let arr = [2,1,5,4,7,5,3];
+// let num = arr.sort();
+// console.log(arr.reverse());
+
+// map
+
+// let arr= [1,2,3,4,5,6];
+// function num(number){
+//     return(number*2);
+// }
+// console.log(arr.map(num));
+
+// filter
+
+// let arr= [1,2,3,4,5,6];
+// function num(number){
+//     return(number%2==0);
+// }
+// console.log(arr.filter(num));
+
+// push
+
+// let user = ['preeti','Alka','Arti','mahima'];
+// let user1 = user.push('kopal');
+// console.log(user);
+
+// let user = ['preeti','Alka','Arti','mahima'];
+// console.log(user.push('kopal'));
+
+// pop
+
+// let num = [1,2,3,4,5,6];
+// let num2 = num.pop();
+// console.log(num);
+
+// let num = [1,2,3,4,5,9];
+// console.log(num.pop());
+
+// shift
+
+// let user = ['preeti','Alka','Arti','mahima'];
+// let user1 = user.shift('kopal');
+// console.log(user);
+
+
+// let user = ['preeti','Alka','Arti','kopal'];
+// console.log(user.shift());
+
+
